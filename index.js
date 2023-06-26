f7affb71f9e7ba2fba058996057e39a5a589e259
José Carlos Marquetti Junior <iwasthesword@users.noreply.github.com>
2023-06-26 09:20:27 -0300

    strip all non-numeric from phone

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use(
 app.get("/rest/1/:bitAPI/crm.lead.add.json", (req, res) => {
   var parameters = req.query; // Get all query parameters
   let ddd = "";
-  let phone = parameters["FIELDS"]["PHONE"][0]["VALUE"];
-  //if (phone.substring(0, 2) === "55") {
-  ddd = phone.substring(0, 2);
-  //}
+  let phone = parameters["FIELDS"]["PHONE"][0]["VALUE"].replace(/\D/g, "");
+  if (phone.substring(0, 2) === "55") {
+    ddd = phone.substring(2, 4);
+  }
   console.log(phone);
   parameters["FIELDS"]["UF_CRM_1687526938"] = ddd;
   const fields = { FIELDS: parameters["FIELDS"] };
